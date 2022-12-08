6406dc335589b79d157c13a42303cb9b939a044e
Tristan F <LeoDog896@hotmail.com>
2022-12-08 09:42:14 -0500

    Begin grabbing

--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
-const url = "https://registry.npmjs.com/-/v1/search?size=250&popularity=1.0&quality=0.0&maintenance=0.0&text=boost-exact:false";
+function buildURL(page: number) {
+    return `https://registry.npmjs.com/-/v1/search?size=250&from=${offset * 250}&popularity=1.0&quality=0.0&maintenance=0.0&text=boost-exact:false`;
+}
+
+const request = await fetch(buildURL(0));
+
+const data = await request.json();
+
+await Deno.writeTextFile("./data.txt", JSON.stringify(data))
