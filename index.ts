25ba1da98cee0d4b9252e45df21f8d79333e9ee9
Tristan F <LeoDog896@hotmail.com>
2022-12-08 10:05:05 -0500

    Fix const issue
    hmm i can only wonder what else can 
    go wrong with using github.dev
    as an editor

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ function buildURL(page: number) {
     return `https://registry.npmjs.com/-/v1/search?size=250&from=${page * 250}&popularity=1.0&quality=0.0&maintenance=0.0&text=boost-exact:false`;
 }
 
-const packages: unknown[] = []
+let packages: unknown[] = []
 
 for (let i = 0; i < 3; i++) {
     const request = await fetch(buildURL(i));
