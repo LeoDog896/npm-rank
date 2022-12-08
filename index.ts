c1a13a9a2c03b4f2360f70dc32fde9997b2dd8a5
Tristan F <LeoDog896@hotmail.com>
2022-12-08 09:44:05 -0500

    Fix reference error

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 function buildURL(page: number) {
-    return `https://registry.npmjs.com/-/v1/search?size=250&from=${offset * 250}&popularity=1.0&quality=0.0&maintenance=0.0&text=boost-exact:false`;
+    return `https://registry.npmjs.com/-/v1/search?size=250&from=${page * 250}&popularity=1.0&quality=0.0&maintenance=0.0&text=boost-exact:false`;
 }
 
 const request = await fetch(buildURL(0));
