6bf2464f1d7988a3e7905a20a92c2c80ee962c65
Tristan F <LeoDog896@hotmail.com>
2022-12-09 16:44:15 -0500

    verification & destruction

--- scripts/index.ts
+++ scripts/index.ts
@@ -1,61 +1,82 @@
 import { z } from "https://deno.land/x/zod@v3.20.0/mod.ts";
+import ProgressBar from "https://deno.land/x/progress@v1.3.4/mod.ts";
 
-function buildURL(page: number) {
-  return `https://registry.npmjs.com/-/v1/search?size=250&popularity=1.0&quality=0.0&maintenance=0.0&text=boost-exact:false&from=${
-    page * 250
-  }`;
-}
+const requestAmount = 40;
+let completed = 0;
 
-const FetchSchema = z.object({
-  objects: z.array(z.object({
-    package: z.object({
-        name: z.string(),
-        version: z.string(),
-        description: z.string().optional(),
-        links: z.object({
-            npm: z.string(),
-            homepage: z.string().optional(),
-            repository: z.string().optional(),
-        }),
-    })
-  })),
+const progress = new ProgressBar({
+	title: "Downloading Packages",
+	total: requestAmount,
 });
 
-type Package = z.infer<typeof FetchSchema>["objects"][number]["package"];
-
-async function getPage(page: number): Promise<Package[]> {
-  console.log("Starting through page " + page);
-  const request = await fetch(buildURL(page));
-
-  const data = await request.json() as unknown;
-
-  const { objects } = FetchSchema.parse(data);
-
-  return objects.map((obj) => obj.package);
+function buildURL(page: number) {
+  // we can get a max of 250 at a time, sorting by popularity only, and using an empty search query (by abusing text filters and using a redundant boost-exact:false filter)
+	return `https://registry.npmjs.com/-/v1/search?size=250&popularity=1.0&quality=0.0&maintenance=0.0&text=boost-exact:false&from=${
+		page * 250
+	}`;
 }
 
-// 40 pages
-const packageRequests = await Promise.allSettled(Array(40).fill(0).map((_, i) => {
-    return getPage(i).then((packages) => ({ page: i, packages }));
-}))
-
-const packages: Package[] = packageRequests.flatMap((req) => {
-    if (req.status === "fulfilled") {
-        console.log(`Page ${req.value.page} was successful`);
-        return req.value.packages;
-    }
-
-    throw req.reason;
+const PackageSchema = z.object({
+  name: z.string(),
+  version: z.string(),
+  description: z.string().optional(),
+  keywords: z.array(z.string()).optional(),
+  publisher: z.object({
+    username: z.string(),
+    email: z.string(),
+  }),
+  maintainers: z.array(z.object({
+    username: z.string(),
+    email: z.string(),
+  })).optional(),
+  links: z.object({
+    npm: z.string(),
+    homepage: z.string().optional(),
+    repository: z.string().optional(),
+  }),
 })
+
+const FetchSchema = z.object({
+	objects: z.array(z.object({
+		package: PackageSchema,
+	})),
+});
+
+type Package = z.infer<typeof PackageSchema>;
+
+async function getPage(page: number): Promise<Package[]> {
+	const request = await fetch(buildURL(page));
+
+	const { objects } = FetchSchema.parse(await request.json());
+
+	return objects.map((obj) => obj.package);
+}
+
+const packageRequests = await Promise.allSettled(
+	Array.from({ length: requestAmount }).map(async (_, i) => {
+		const packages = await getPage(i);
+		completed++;
+		progress.render(completed);
+		return ({ page: i, packages });
+	}),
+);
+
+const packages: Package[] = packageRequests.flatMap((req, i) => {
+	if (req.status === "rejected") {
+		console.error(`Failed to fetch page ${i}.`);
+		Deno.exit(1);
+	}
+	return req.value.packages;
+});
 
 await Deno.writeTextFile("./raw.txt", JSON.stringify(packages));
 
 function optionallyFormat(arg: string | undefined, label: string): string {
-  if (!arg) {
-    return "";
-  }
+	if (!arg) {
+		return "";
+	}
 
-  return ` ([${label}](${arg}))`;
+	return ` ([${label}](${arg}))`;
 }
 
 const mdContent = `# Packages
@@ -63,13 +84,13 @@ const mdContent = `# Packages
 Ordered list of top 10000 NPM packages:
 
 ${
-  packages.map((pkg) =>
-    `- [${pkg.name}](${pkg.links.npm})
-    - ${pkg.description}
-    - v${pkg.version} ${optionallyFormat(pkg.links.homepage, "homepage")}${
-      optionallyFormat(pkg.links.repository, "repository")
-    }`
-  ).join("\n")
+	packages.map(({ name, links: { npm, homepage, repository }, description, version }) =>
+		`- [${name}](${npm})
+    - ${description}
+    - v${version} ${optionallyFormat(homepage, "homepage")}${
+			optionallyFormat(repository, "repository")
+		}`
+	).join("\n")
 }
 `;
 
